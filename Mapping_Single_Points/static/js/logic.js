83102ce5292cc84eff1c0962b53abdfa6a9bf7e3
Abigayle Reeves <16areeves@gmail.com>
2022-06-03 20:02:01 -0400

    pushing marker code

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // // Create the map object with a center and zoom level.
 // let map = L.map("mapid", {
@@ -12,6 +12,13 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });      this is another way to initialize a map, outlining the lat and long coords and the zoom level (b/t 0-18)
               // useful when adding multiple layers to a map
+
+// Add marker to tthe map for LA, California //// Can use the circleMarker f(x), has the same syntax but the circle behaves differently when zooming out
+let marker = L.circle([34.0522, -118.2437], {
+    radius: 1000,
+    fillColor: "yellow",
+    color: "black"
+ }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
